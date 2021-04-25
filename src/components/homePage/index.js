f747337ad839f91b9bc0d39b7d54ca44de9a5f45
Satyaki Das <thesatyakidas.00220033@gmail.com>
2021-04-25 10:23:33 +0530

    Revert "Login Page Updated (#1)" (#2)
    This reverts commit af4eb915b34b39e7d3a3e5df4479d038d4170df0.

--- src/components/homePage/index.js
+++ src/components/homePage/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
 class Home extends React.Component {
     render() {
-      return;
+      return(
+        <>
+          {/* Write  your code here */}
+          <h1>Home Page</h1>
+        </>
+      );
     }
   }
 export default Home
