4843a906f445dd7301804c3d4893ea16ee1dbc3e
kaustav-deb <68233780+kaustav-deb@users.noreply.github.com>
2021-04-25 21:18:24 +0530

    home-login-register added
    * home/login/register
    
    * changes#2
    
    * Added package.json

--- src/components/homePage/index.js
+++ src/components/homePage/index.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import { Layout, Menu, Card } from 'antd';
+import './style.css'
+
+const { Header, Content, Footer } = Layout;
 class Home extends React.Component {
     render() {
       return(
         <>
-          {/* Write  your code here */}
-          <h1>Home Page</h1>
-        </>
+          <Layout className="layout">
+    <Header>
+     
+     
+      <Menu className="menu" theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">REGISTER  <a href="/register"></a></Menu.Item>
+        <Menu.Item key="2">LOGIN <a href="/login"></a></Menu.Item>
+       </Menu>
+    </Header>
+   
+    
+    <div className="bg">
+      <Card className="card" style={{ width: 1000 }}>
+        <p>Team 1 vs Team 2 </p>
+        
+  </Card></div>
+    <Footer style={{ textAlign: 'center' }}></Footer>
+  </Layout>
+          </>
       );
     }
   }
