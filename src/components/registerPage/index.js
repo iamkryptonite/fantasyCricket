af8a415890629048856e2a994ec663c7cebf4524
Satyaki Das <thesatyakidas.00220033@gmail.com>
2021-04-25 10:21:29 +0530

    Revert "Login Page Updated (#1)"
    This reverts commit af4eb915b34b39e7d3a3e5df4479d038d4170df0.

--- src/components/registerPage/index.js
+++ src/components/registerPage/index.js
@@ -1,26 +1,12 @@
 import React from 'react'
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
 class Register extends React.Component {
     render() {
         return(
-          <MDBContainer>
-            <MDBRow>
-              <MDBCol md="6">
-                <form>
-                  <p className="h5 text-center mb-4">Sign up</p>
-                  <div className="grey-text">
-                    <MDBInput label="Your username" icon="user" group type="text" validate error="wrong"
-                      success="right" />
+            <>
+              {/* Write  your code here */}
+              <h1>Register Page</h1>
 
-                    <MDBInput label="Your password" icon="lock" group type="password" validate />
-                  </div>
-                  <div className="text-center">
-                    <MDBBtn color="primary">Register</MDBBtn>
-                  </div>
-                </form>
-              </MDBCol>
-            </MDBRow>
-            </MDBContainer>
+            </>
           );
     }
   }
